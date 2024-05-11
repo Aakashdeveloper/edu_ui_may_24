b30ad61bd92161e839cd9471b44956b01076657a
aakashdeveloper <ahanda205@gmail.com>
2024-05-11 23:19:50 +0530

    boot5

---
+++ javascript/datatype.js
@@ -0,0 +1,119 @@
+es5> Ecma Script5
+
+es6> Ecma Script6
+// Datatypes
+String > "hiii"  'hiii'  "10" 'true' `true` `test`
+Number > 10  333434  4534.435  .34545
+Boolean> true false
+
+
+//es5
+var a = 10
+var b = "Hii"
+var c = true
+
+typeof(a)
+'number'
+typeof(b)
+'string'
+typeof(c)
+'boolean'
+var d= 10.11
+undefined
+typeof(d)
+'number'
+var e = "10"
+undefined
+typeof(e)
+'string'
+
+
+//////////
+var a = 10
+var b = 5
+a+b
+15
+a-b
+5
+a*b
+50
+a/b
+2
+
+a%b
+4%2
+0
+4%3
+1
+
+2%3
+2
+3%4
+3
+
+var a = "Hii"
+var b = "Javascript"
+a+b
+'HiiJavascript'
+
+a-b
+NaN (Not a number)
+a*b
+NaN
+a/b
+NaN
+
+string + string = string
+string + number = string
+number + string = string
+number + number = number
+
+10+"20"+30 
+"1020"+30
+"102030"
+
+10+20+"30"
+30+"30"
+"3030"
+
+10+"20"+"30"
+"1020"+"30"
+"102030"
+
+10+"20"+30-1
+102029
+10+20+"30"-1
+3029
+10+"20"+"30"-1
+102029
+
+"hi"-1
+NaN
+"10"-1
+9
+2*"43"
+86
+"4"/"2"
+2
+"4a"/"2"
+NaN
+2*"43b"
+NaN
+"10"+"1"
+'101'
+
+var a = true
+var b = true
+a+b
+2
+
+
+10+true
+11
+10/false
+Infinity
+true-false
+1
+
+true is equal to 1
+false is equal to 0
